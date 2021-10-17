c624846281594e93e17aadd2e6ee76f8da549cf0
Erik Stone <spologas@gmail.com>
2021-10-17 11:50:57 -0700

    added some coding in the wrong place, commented it out, added bcrypt to npm.

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,36 @@
 const User = require('./User');
 
+// const bcrypt = require('bcrypt');
+
+// userSchema = new Schema({
+//     username: {
+//         type: String,
+//         required: true,
+//         unique: true,
+//     },
+    
+//     email: { type: String, required: true, unique: true, match: [/.+@.+\..+/, "Please enter a valid email address"] },
+//     password: { type: String, required: true},
+//     savedBooks: [bookSchema],
+//     },
+//     {toJSON: {
+//         virtuals: true,
+//     },}
+// )
+// // salt and hashing password
+// const saltRounds = 10;
+// userSchema.pre('save', async function (next) {
+//     if (this.isNew || this.isModified('password')) {
+//         this.password = await bcrypt.hash(this.password, saltRounds);
+//     }
+//     next();
+// });
+
+// userSchema.methods.usCorrect = async function (password) {
+//     return bcrypt.compare(password, this.password);
+// };
+// userSchema.virtual('bookCount').get(function (){
+//     return this.savedBooks.length;
+// });
+// const User = model('User', userSchema);
 module.exports = { User };
